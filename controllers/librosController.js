ecc9ec280c471db503aa2a5b855792988e6550f5
mjc14872 <14872@uagro.mx>
2022-05-08 22:51:18 -0500

    creacion de encabezado, footer y un poco de estilo css

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -1,7 +1,13 @@
+let db = require("../database/models");
+
 let librosController = {
     crear: function (req, res) {
-        
+        db.Genero.findAll()
+            .then(function(generos) {
+                return res.render("creacionLibros", {generos:generos});
+            })
     }
 }
+
 
 module.exports = librosController;
